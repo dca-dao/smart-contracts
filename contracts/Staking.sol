9cbfa412a1c5f89b2d362e86d2e8fdf8182fb4d2
Louis <louis.dutoitld@gmail.com>
2022-05-28 21:07:12 +0200

    Aaave lending pool interface implementation

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,3 +1,20 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.6;
+
+contract Staking {
+
+  ILendingPool lendingPool = ILendingPool(provider.getLendingPool());
+
+  // Transfer funds from the caller to the depositing contract
+  IERC20(asset).transferFrom(msg.sender, address(this), amount);
+
+  // Approve LendingPool to spend your contracts funds
+  if (IERC20(asset).allowance(address(this), address(LendingPool)) == 0) {
+    erc20.approve(address(LendingPool), uint256(-1));
+  }
+
+  // Deposit on onBehalf of msg.sender
+  lendingPool.deposit(_asset, _amount, msg.sender, 0);
+
+}
