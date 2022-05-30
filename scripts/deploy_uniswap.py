19f331c223aba957f0dae021754c0fde9a9b36e4
Pierre Fontaine <pierrefontaine00@gmail.com>
2022-05-30 11:58:05 +0200

    Add Keeper implementation with Uniswap swap

--- scripts/deploy_uniswap.py
+++ scripts/deploy_uniswap.py
@@ -1,4 +1,4 @@
-from brownie import SwapExamples
+from brownie import SwapExamples, DcaKeeper
 from scripts.useful_scripts import get_account
 from scripts.fund_and_withdraw import fund_contract, withdraw_contract
 
@@ -13,6 +13,17 @@ def deploy_dao():
     return dca_dao
 
 
+def deploy_dca_keeper():
+    account = get_account()
+    keeper_exemple = DcaKeeper.deploy(
+        300,
+        "0xE592427A0AEce92De3Edee1F18E0157C05861564",
+        {"from": account},
+        publish_source=True,
+    )
+    return keeper_exemple
+
+
 def approuve_contract():
     # SwapExamples[0].approve(
     #    "0xaD6D458402F60fD3Bd25163575031ACDce07538D", 100, {"from": get_account()}
@@ -21,4 +32,4 @@ def approuve_contract():
 
 
 def main():
-    deploy_dao()
+    deploy_dca_keeper()
