47060f879a0f685cbcee247e29eed2743e9e3c03
Louis <louis.dutoitld@gmail.com>
2022-05-31 08:34:36 +0200

    weth interface + lending

---
+++ scripts/aave_lending.py
@@ -0,0 +1,6 @@
+def lending():
+    account = get_account()
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in ["mainnet-fork"]:
+        get_weth(account=account)
+    lending_pool = get_lending_pool()
